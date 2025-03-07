791394ad240e720e541912ed4734fdf3170bac7e
Vasu_Devan_S <vasu.devan.skj@gmail.com>
2025-03-07 19:56:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
-import spacy
+# import spacy
 
 # Load spaCy model
-nlp = spacy.load('en_core_web_sm')
+# nlp = spacy.load('en_core_web_sm')
 
 # Define Streamlit app
 def main():
     st.title('My spaCy app')
     text = st.text_input('Enter some text:')
-    doc = nlp(text)
-    for token in doc:
-        st.write(token.text, token.pos_, token.dep_)
+    # doc = nlp(text)
+    # for token in doc:
+        # st.write(token.text, token.pos_, token.dep_)
+    st.write("\n".join(text.split()))
+
 
 if __name__ == '__main__':
     main()
